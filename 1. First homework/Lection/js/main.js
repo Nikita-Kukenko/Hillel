6b9bee72ce623d0e03c0887d43da0d0453f083ae
Nikita-Kukenko <kukenkonik@gmail.com>
2020-08-31 14:12:12 +0300

    finished lection task

---
+++ 1. First homework/Lection/js/main.js
@@ -0,0 +1,10 @@
+a = prompt('Введите число "a"', '');
+b = prompt('Введите число "b"', '');
+
+console.log('**************');
+console.log('При а = ' + a + ', b = ' + b);
+console.log('Результат:');
+console.log('--------------------');
+console.log('a + b = ' + (+a + +b) + '; a - b = ' + (a - b) + '; a * b = ' + (a * b) + '; a / b = ' + (a / b) + ';');
+console.log('-------------------');
+console.log('**************');
