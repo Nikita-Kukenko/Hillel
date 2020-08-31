287fbf7aaa068779b26723c13dc414363f6a80e1
Nikita-Kukenko <kukenkonik@gmail.com>
2020-08-31 11:42:03 +0300

    fixed bags

--- 1. First homework/2nd task/js/main.js
+++ 1. First homework/2nd task/js/main.js
@@ -1,11 +1,11 @@
-firstUserInfo = prompt('Введите возраст, Имя и Фамилию', '18 лет Юрий Иванов' );
-secondUserInfo = prompt('Введите возраст, Имя и Фамилию', '16 лет Дмитрий Кузьмин' );
-thirdUserInfo = prompt('Введите возраст, Имя и Фамилию', '19 лет Оксана Петрова' );
+firstUserInfo = prompt('Введите возраст, Имя и Фамилию', '18 лет Юрий Иванов');
+secondUserInfo = prompt('Введите возраст, Имя и Фамилию', '16 лет Дмитрий Кузьмин');
+thirdUserInfo = prompt('Введите возраст, Имя и Фамилию', '19 лет Оксана Петрова');
 
 console.log('**************');
 console.log('Список студентов зарегестрировавшихся на вебинар:');
-console.log('1.' + firstUserInfo);
-console.log('2.' + secondUserInfo);
-console.log('3.' + thirdUserInfo);
-console.log('-------------- Средний возраст студента: ' + ((parseInt(firstUserInfo) + parseInt(secondUserInfo) + parseInt(thirdUserInfo)) / 3) + '--------------');
+console.log('1. ' + firstUserInfo);
+console.log('2. ' + secondUserInfo);
+console.log('3. ' + thirdUserInfo);
+console.log('-------------- Средний возраст студента: ' + ((parseInt(firstUserInfo) + parseInt(secondUserInfo) + parseInt(thirdUserInfo)) / 3) + ' --------------');
 console.log('**************');
