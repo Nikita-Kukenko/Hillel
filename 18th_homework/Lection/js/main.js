9e45624e141fc3bd0fb77d06e79835b8214dfc25
Nikita-Kukenko <kukenkonik@gmail.com>
2020-11-19 20:20:36 +0200

    fixed lection task

--- 18th_homework/Lection/js/main.js
+++ 18th_homework/Lection/js/main.js
@@ -1,18 +1,22 @@
 const box = document.querySelector('.box');
 
-box.onmousedown = function(event) {
-  document.addEventListener('mousemove', goElem);
+function initDragAndDrop(item) {
+  item.onmousedown = function() {
+    document.addEventListener('mousemove', goElem);
+  }
+  
+  function moveElem (x, y) {
+    item.style.left = `${x - (item.offsetWidth / 2)}px`;
+    item.style.top = `${y - (item.offsetHeight / 2)}px`;
+  }
+  
+  function goElem(event) {
+    moveElem(event.pageX, event.pageY);
+  }
+  
+  document.onmouseup = function() {
+    document.removeEventListener('mousemove', goElem);
+  }
 }
 
-function moveElem (x, y) {
-  box.style.left = `${x - (box.offsetWidth / 2)}px`;
-  box.style.top = `${y - (box.offsetHeight / 2)}px`;
-}
-
-function goElem(event) {
-  moveElem(event.pageX, event.pageY);
-}
-
-document.onmouseup = function() {
-  document.removeEventListener('mousemove', goElem);
-}
+dragAndDrop(box);
